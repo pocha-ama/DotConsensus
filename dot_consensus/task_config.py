a25e986ae6a6694ecf32a34ba2a6140e82046615
TAKENISHI-Kaito <kaito.takenishi@gmail.com>
2026-06-24 22:32:59 +0900

    modify PILOT_TASK

--- dot_consensus/task_config.py
+++ dot_consensus/task_config.py
@@ -1,6 +1,6 @@
 PILOT_TASKS = [
-    # dict(task_type='dot', choice_type='redblue', can_review=False, answer='red'),
-    # dict(task_type='dot', choice_type='redblue', can_review=False, answer='blue'),
+    dict(task_type='dot', choice_type='redblue', can_review=False, answer='red'),
+    dict(task_type='dot', choice_type='redblue', can_review=False, answer='blue'),
 
     dict(task_type='gabor', choice_type='interval', can_review=False, answer='first'),
     dict(task_type='gabor', choice_type='interval', can_review=False, answer='second'),
